c48ea49b7e27d5bd2d70bcf3afee4560b758b372
nichena01024 <32244999+nichena01024@users.noreply.github.com>
2019-04-22 19:51:59 -0400

    创建简版webpack ssr配置base部分

---
+++ config/webpack.base.config.js
@@ -0,0 +1,31 @@
+const webpackConfig = {
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx|js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader', options: { modules: true, importLoaders: 1 } },
+                ],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(png|jpeg|jpg|gif|svg)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    publicPath: '/'
+                }
+            },
+        ]
+    }
+};
+
+module.export = webpackConfig;
