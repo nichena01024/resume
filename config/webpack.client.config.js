7e4ca55c9dcfbf9b97a3288ad9ca87a5f5336bc6
nichena01024 <32244999+nichena01024@users.noreply.github.com>
2019-04-22 21:46:14 -0400

    创建ssr webpack配置文件client部分

---
+++ config/webpack.client.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base.config');
+
+const webpackConfig = merge(baseConfig, {
+    mode: 'development',    // TODO：自动更改
+    entry: './src/client/index.js',
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'public')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css?$/,
+                use: ['style-loader', {
+                    loader: 'css-loader',
+                    options: {
+                        importLoaders: 1,
+                        modules: true,
+                        localIdentName: '[name]_[local]_[hash:base64:5]'
+                    }
+                }]
+            },
+            {
+                test: /\.(png|jpeg|jpg|gif|svg)?$/,
+                loader: 'url-loader',   // 可优化，会打包两遍
+                options: {
+                    limit: 10000,
+                    publicPath: '/'
+                }
+            },
+        ]
+    }
+});
+
+module.exports = webpackConfig;
